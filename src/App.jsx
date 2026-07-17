241d4094b304e0cfd2c0c328828cfee2c8b7d315
JoeMichel22 <92042938+JoeMichel22@users.noreply.github.com>
2026-07-17 17:12:40 +0000

    Created and added Hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App(){
   return (
     <>
       <Navbar/>
+      <Hero/>
 
-      <main> 
+      {/* <main> 
         <h2>Welcome to the Recipe Hub</h2>
 
         <p>
           Discover delicious meals and drinks.
         </p>
-      </main> 
+      </main>  */}
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import "../styles/Hero.css"
+
+function Hero(){
+
+    return(
+        <section className="hero">
+            <div className="hero-content">
+                
+                <h1>Discover Delicious Recipes</h1>
+
+                <p>Explore meals, drinks, and recipes from around the world</p>
+
+                <button>Explore Recipes</button>
+
+            </div>
+        </section>
+    );
+
+}
+
+export default Hero;
