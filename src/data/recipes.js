e179cf99aee66321c2427fd2c70f87230b503fea
JoeMichel22 <92042938+JoeMichel22@users.noreply.github.com>
2026-07-30 19:04:20 +0000

    Added first food images and data js page

---
+++ src/data/recipes.js
@@ -0,0 +1,9 @@
+const recipes =[
+    {id:1, title:"Chicken Alfredo", category:"Dinner", image:"../assets/images/chicken_alfredo.png", description:"Creamy pasta with grilled chicken"},
+    {id:2, title:"Haitian Griot", category:"Caribbean", image:"../assets/images/griot.png", description:"Tradition Haitian fried pork"},
+    {id:3, title:"Mango Smoothie", category:"Drinks", image:"../assets/images/mango_smoothie.png", description:"Fresh blended mango drink"},
+    // {id:4, title:, category:, image:, description:},
+    // {id:5, title:, category:, image:, description:}
+];
+
+export default recipes;
